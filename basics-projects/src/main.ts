bb2bf233053e91b800353aa0c7e56e661f610027
Nimira43 <n.rabone72@aol.com>
2024-12-14 16:15:08 +0000

    Orthographic Camera script archived. Animation loop demo

--- basics-projects/src/main.ts
+++ basics-projects/src/main.ts
@@ -1,12 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
-scene.add(new THREE.GridHelper())
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 1.5
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -18,38 +18,22 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
+new OrbitControls(camera, renderer.domElement)
+
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-cube.position.y = 0.5
 scene.add(cube)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const gui = new GUI()
-
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'x', -10, 10)
-cameraFolder.add(camera.position, 'y', -10, 10)
-cameraFolder.add(camera.position, 'z', -10, 10)
-cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'far', 0.01, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.open()
-
 function animate() {
   requestAnimationFrame(animate)
+
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
 
   renderer.render(scene, camera)
 
