fab2de7c0865bdd0693fa535a56bbc14eb41fc8d
Nimira43 <n.rabone72@aol.com>
2024-12-14 16:49:28 +0000

    First project of this section has a cube which can be rotated, positioned and scaled with debug co-ordinates

--- geometries-and-materials-projects/src/main.ts
+++ geometries-and-materials-projects/src/main.ts
@@ -5,13 +5,25 @@ import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
+camera.position.set(-2, 4, 5)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement)
+const boxGeometry = new THREE.BoxGeometry()
+
+const material = new THREE.MeshNormalMaterial({
+  wireframe: true,
+})
+
+const cube = new THREE.Mesh(boxGeometry, material)
+// cube.position.x = -4
+scene.add(cube)
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
@@ -19,37 +31,36 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-new OrbitControls(camera, renderer.domElement)
-
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshNormalMaterial({ wireframe: true })
-
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
 const gui = new GUI()
 
 const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+const cubeRotationFolder = cubeFolder.addFolder('Rotation')
+cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01)
+cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01)
+cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01)
+const cubePositionFolder = cubeFolder.addFolder('Position')
+cubePositionFolder.add(cube.position, 'x', -10, 10)
+cubePositionFolder.add(cube.position, 'y', -10, 10)
+cubePositionFolder.add(cube.position, 'z', -10, 10)
+const cubeScaleFolder = cubeFolder.addFolder('Scale')
+cubeScaleFolder.add(cube.scale, 'x', -5, 5, 0.1) // .onFinishChange(function() { console.log(cube.geometry) })
+cubeScaleFolder.add(cube.scale, 'y', -5, 5, 0.1)
+cubeScaleFolder.add(cube.scale, 'z', -5, 5, 0.1)
+
 cubeFolder.open()
 
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 20)
-cameraFolder.open()
-
+const debug = document.getElementById('debug') as HTMLDivElement
 
 function animate() {
   requestAnimationFrame(animate)
 
-  // cube.rotation.x += 0.01
-  // cube.rotation.y += 0.01
-
   renderer.render(scene, camera)
+
+  debug.innerText = 'Matrix\n' + cube.matrix.elements.toString().replace(/,/g, '\n')
+
   stats.update()
 }
 
